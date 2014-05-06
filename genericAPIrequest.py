62f3cfcf4012b68edba20ab1896efc2843191aa8
sublime <umbsublime@gmail.com>
2014-05-06 01:28:33 -0400

    Starting writting master plan

--- genericAPIrequest.py
+++ genericAPIrequest.py
@@ -29,3 +29,111 @@ class AbstractRequest(metaclass=ABCMeta):
         self.ParseData()
 
         return self.data
+
+def main():
+    test = AbstractRequest()
+
+if __name__ == '__main__':
+    main()
+
+'''
+HeroRequest just returns EVERYTHING about the hero
+    Hero will use it to fill itself
+        Hero_container will contain all the heroes
+            ProfileContainer will contain everything about all heroes and profile and manipulate Hero_container
+
+
+AbstractRequest()
+    HeroRequest()
+    ItemRequest()
+    ProfileRequest()
+
+AbstractElement()
+    +SendRequest()
+    +ParseData()
+    self.raw_data
+
+
+    Skill():
+
+
+    Hero()
+    +GetItemUrl()
+    self.item_url_collection[]
+    self.skill_name_list
+    self.
+
+
+    Item()
+    self.type
+    self.modifier
+    self.tooltip
+
+        Gem():
+        Weapon():
+        Equipment():
+            Arm():
+            Shield():
+             . . .
+            Legs()
+
+    Profile()
+    +
+
+AbstractContainer
+    HeroContainer()
+
+'''
+class HeroRequest(AbstractRequest):
+
+    apiProfileBaseUrl = 'http://us.battle.net/api/d3/profile/{}-{}/hero/{}'
+
+    def __init__(self, userName=None, userId=None, hero_id=None):
+        if userName is None:
+            raise ValueError('ItemRequest init: missing userName')
+        if userId is None:
+            raise ValueError('ItemRequest init: missing userId')
+
+        if hero_id is None:
+            raise ValueError('ItemRequest init: missing query')
+
+        self.userName = userName
+        self.userId = str(userId)
+        self.query = hero_id
+
+        self.url = apiProfileBaseUrl.format(self.userName, self.userId, self.query)
+        print ('Debug: {}'.format(self.url))
+
+    def ParseData(self):
+        self.items = {}
+
+        # Get stats
+        self.stats = self.jsonData['stats']
+
+        # Get items
+        for item in self.jsonData['items']:
+            try:
+                self.items[item] = {'name': self.jsonData['items'][item]['name'],
+                                    'link': self.jsonData['items'][item]['tooltipParams']}
+            except KeyError:
+                pass
+
+        # Get skills
+        self.skills = {}
+        for skill in self.jsonData['skills']['active']:
+            try:
+                self.skills[skill['skill']['name']] = {'rune': skill['rune']['description'],
+                                                       'skill': skill['skill']['description']}
+            except KeyError:
+                pass
+
+        self.data = {'stats':self.stats, 'skills': self.skills, 'items': self.items}
+
+    def GetSkills(self):
+        return self.skills
+
+    def GetItems(self):
+        return self.items
+
+    def GetStats(self):
+        return self.stats
